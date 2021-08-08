d12cf2a3001fc285362b3d64d819888836f09136
nikhilAndola <nikhilandola123@gmail.com>
2021-08-08 23:06:19 +0530

    Project in final stage

--- to_do_list.js
+++ to_do_list.js
@@ -1,5 +1,4 @@
-add = document.getElementById("add");
-add.addEventListener("click",()=>{
+function getAndUpdate(){
     console.log("Updating List...");
     tit = document.getElementById("title").value;
     desc = document.getElementById("description").value;
@@ -13,6 +12,17 @@ add.addEventListener("click",()=>{
         itemJsonArray.push([tit, desc]);
         localStorage.setItem('itemsJson', JSON.stringify(itemJsonArray));
     }
+    update();
+}
+
+function update(){
+    if(localStorage.getItem('itemsJson')==null){
+        itemJsonArray = [];
+        localStorage.setItem('itemsJson', JSON.stringify(itemJsonArray));
+    }else {
+        itemJsonArrayStr = localStorage.getItem('itemsJson');
+        itemJsonArray = JSON.parse(itemJsonArrayStr);
+    }
     //Populate the table
     let tableBody = document.getElementById('tableBody');
     let str = "";
@@ -21,8 +31,21 @@ add.addEventListener("click",()=>{
         <th scope="row">${index+1}</th>
         <td>${element[0]}</td>
         <td>${element[1]}</td>
-        <td><button class="btn btn-sm btn-primary">Delete</button></td>
+        <td><button class="btn btn-sm btn-primary" onclick="deleted(${index})">Delete</button></td>
       </tr>`
     });
     tableBody.innerHTML = str;
-}); 
+}
+
+add = document.getElementById("add");
+add.addEventListener("click",getAndUpdate); 
+update();
+function deleted(itemIndex){
+    console.log("Delete", itemIndex);
+    itemJsonArrayStr = localStorage.getItem('itemsJson');
+    itemJsonArray = JSON.parse(itemJsonArrayStr);
+    // Delete itemIndex element from the array
+    itemJsonArray.splice(itemIndex, 1);
+    localStorage.setItem('itemsJson', JSON.stringify(itemJsonArray));
+     update();
+}
